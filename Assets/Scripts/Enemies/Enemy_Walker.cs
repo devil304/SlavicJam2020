f47db84b967be014d7bd3d924c7392c8128542c6
Laguland <s.skubis1@unimail.derby.ac.uk>
2020-07-25 00:48:56 +0200

    first enemy

---
+++ Assets/Scripts/Enemies/Enemy_Walker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Walker : Enemy_Base
+{
+    [SerializeField] private GameObject WaypointsContainer;
+    [SerializeField] private List<GameObject> waypoints;
+    private int waypointIndex = 0;
+    private bool isMovementFinished = false;
+    private void Awake()
+    {
+        WaypointsContainer.SetActive(false);
+    }
+
+    float time = 2f;
+    private void Update()
+    {
+        if(!isMovementFinished)
+        {
+            time -= Time.deltaTime;
+            if (time < 0)
+            {
+                time = 2f;
+
+                var direction = waypoints[waypointIndex].transform.position - transform.position;
+
+                MoveToDirection(direction.normalized, StepFinished);
+            }
+        }
+    }
+
+    void StepFinished()
+    {
+        if(waypointIndex < waypoints.Count)
+        {
+            if (transform.position == waypoints[waypointIndex].transform.position)
+                waypointIndex++;
+        } 
+        else if(waypointIndex == waypoints.Count)
+        {
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                isMovementFinished = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WaypointShoot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject Direction;
+
+    public Vector2 ShootDirection { get ; set; }
+
+    private void Start()
+    {
+        ShootDirection = new Vector2(Direction.transform.up.x, Direction.transform.up.y);
+    }
+}
