ba63c981123329df24854da6fd1ea2ce5ce22cbe
Szymon Rzosinski <szymonrzosinski@gmail.com>
2020-07-24 22:35:45 +0200

    Clock and part of player

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void BeatDel();
+public class Clock : MonoBehaviour
+{
+    [SerializeField] float beatTime = 0.5f;
+    public static event BeatDel Beat;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(beat());
+    }
+
+    IEnumerator beat()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(beatTime);
+            Beat?.Invoke();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2Int currentPos;
+    MainInput MI;
+    Vector2Int moveDir = new Vector2Int(0,0);
+    bool moving = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MI = new MainInput();
+        MI.Enable();
+        MI.Movement.updown.performed += perf => {
+            if (!moving)
+            {
+                moving = true;
+                StartCoroutine(Move());
+            }
+        };
+        MI.Movement.leftright.performed += perf => {
+            if (!moving)
+            {
+                moving = true;
+                StartCoroutine(Move());
+            }
+        };
+
+        Clock.Beat += () => {
+            BeatT = true;
+            Debug.Log("Clock");
+            i++;
+        };
+
+    }
+    bool BeatT = false;
+    int i = 0, x = 0;
+    IEnumerator Move()
+    {
+
+        do
+        {
+            moveDir = new Vector2Int((int)MI.Movement.leftright.ReadValue<float>(), (int)MI.Movement.updown.ReadValue<float>());
+            Debug.Log("Move : "+moveDir);
+            yield return new WaitUntil(() => BeatT);
+            if(x==0)
+                x = i;
+            else
+                x++;
+            BeatT = false;
+            if (x < i - 1)
+            {
+                Debug.LogWarning("Skip i: "+i+" x: "+x);
+                x = i;
+            }
+        } while (moveDir != Vector2Int.zero);
+        moving = false;
+        x = 0;
+    }
+}
