2d602c4eb8040c2778462853091bc76474563c2b
Szymon Rzosinski <szymonrzosinski@gmail.com>
2020-07-24 20:54:57 +0200

    Pierdolety v0.1

---
+++ Assets/Scripts/MapMapper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class MapMapper : MonoBehaviour
+{
+    [HideInInspector] public TileInfo[,] map { get; private set; }
+
+    public void mapTheMap()
+    {
+       List<Transform> tiles = new List<Transform>();
+
+        foreach(Transform child in transform)
+        {
+            if(child.GetComponent<TileInfo>())
+                tiles.Add(child);
+        }
+
+        int maxX = (int)Math.Round(tiles.Max(go => go.position.x), 0);
+        int maxY = (int)Math.Round(tiles.Max(go => go.position.y), 0);
+
+        map = new TileInfo[maxX+1, maxY+1];
+
+        foreach (Transform go in tiles)
+        {
+            map[(int)Math.Round(go.position.x,0), (int)Math.Round(go.position.y,0)] = go.GetComponent<TileInfo>();
+        }
+    }
+
+    private void Awake()
+    {
+        mapTheMap();
+    }
+}
---
+++ Assets/Scripts/TileInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class TileInfo : MonoBehaviour
+{
+    public bool HasTable = false;
+    public TileType TType;
+}
+
+public enum TileType { Wall, Floor };
