f37fe356577df3f27a760d57d9c34ef0af87fe34
Szymon Rzosinski <szymonrzosinski@gmail.com>
2020-07-24 23:10:54 +0200

    Bardzo krzywy player v0.1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,9 +9,11 @@ public class Player : MonoBehaviour
     MainInput MI;
     Vector2Int moveDir = new Vector2Int(0,0);
     bool moving = false;
+    MapMapper MM;
     // Start is called before the first frame update
     void Start()
     {
+        MM = FindObjectOfType<MapMapper>();
         MI = new MainInput();
         MI.Enable();
         MI.Movement.updown.performed += perf => {
@@ -39,21 +42,27 @@ public class Player : MonoBehaviour
     int i = 0, x = 0;
     IEnumerator Move()
     {
-
+        var tmp = MI.Movement;
         do
         {
-            moveDir = new Vector2Int((int)MI.Movement.leftright.ReadValue<float>(), (int)MI.Movement.updown.ReadValue<float>());
+            moveDir = new Vector2Int(tmp.leftright.ReadValue<float>()>0?(int)Math.Ceiling(tmp.leftright.ReadValue<float>()): (int)Math.Floor(tmp.leftright.ReadValue<float>()),
+                tmp.updown.ReadValue<float>() > 0 ? (int)Math.Ceiling(tmp.updown.ReadValue<float>()) : (int)Math.Floor(tmp.updown.ReadValue<float>()));
             Debug.Log("Move : "+moveDir);
+            BeatT = false;
             yield return new WaitUntil(() => BeatT);
             if(x==0)
                 x = i;
             else
                 x++;
-            BeatT = false;
             if (x < i - 1)
             {
                 Debug.LogWarning("Skip i: "+i+" x: "+x);
                 x = i;
+            }
+            if (!(moveDir.x != 0 && moveDir.y != 0) && MM.map[currentPos.x + moveDir.x, currentPos.y + moveDir.y].TType == TileType.Floor)
+            {
+                transform.position += (Vector3Int)moveDir;
+                currentPos += moveDir;
             }
         } while (moveDir != Vector2Int.zero);
         moving = false;
