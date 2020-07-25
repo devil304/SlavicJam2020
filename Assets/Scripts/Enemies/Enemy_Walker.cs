9663861567f816d24516d91e39f99aec7811dd94
Szymon Rzosiński <szymonrzosinski@gmail.com>
2020-07-25 02:06:29 +0200

    Comments Enemy_Walker

--- Assets/Scripts/Enemies/Enemy_Walker.cs
+++ Assets/Scripts/Enemies/Enemy_Walker.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Enemy_Walker : Enemy_Base
 {
     [SerializeField] private GameObject WaypointsContainer;
-    [SerializeField] private List<GameObject> waypoints;
-    private int waypointIndex = 0;
+    [SerializeField] private List<GameObject> waypoints; //Array zajmuje mniej i jest szybszy, a nie modyfikujesz w kodzie listy
+    private int waypointIndex = 0; //w C# domyślnie zmienne są private lub internal, więc można nie pisać ;)
     private bool isMovementFinished = false;
     private void Awake()
     {
@@ -31,7 +31,7 @@ public class Enemy_Walker : Enemy_Base
     {
         if(waypointIndex < waypoints.Count)
         {
-            var dist = Vector3.Distance(transform.position, waypoints[waypointIndex].transform.position);
+            var dist = Vector3.Distance(transform.position, waypoints[waypointIndex].transform.position); //Vector2.Distance byłby lepszy bo nie bierzemy pod uwagę osi z (Vector2 i Vector3 mają bezpośrednie konwersje) 
             if (dist < 1.1)
             {
                 waypointIndex++;
