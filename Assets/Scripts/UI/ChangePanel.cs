6749913f95f9e26c820a4ce527924d3ef1433326
Invighost <pawelrzosinski@gmail.com>
2020-07-25 14:32:43 +0200

    UI management

---
+++ Assets/Scripts/UI/ChangePanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ChangePanel : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    public void action()
+    {
+        Transform current = gameObject.GetComponent<Transform>();
+        while (current.parent.parent != null)
+        {
+            current = current.parent;
+        }
+        panel.SetActive(true);
+        current.gameObject.SetActive(false);
+    }
+}
