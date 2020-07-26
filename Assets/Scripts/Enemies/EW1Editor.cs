a9dcb445f813fe686e895398fefe0aca947cd316
Szymon Rzosinski <szymonrzosinski@gmail.com>
2020-07-26 14:50:25 +0200

    fdzasfsddsfsdf

--- Assets/Scripts/Enemies/EW1Editor.cs
+++ Assets/Scripts/Enemies/EW1Editor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -78,3 +79,4 @@ public class EW1Editor : Editor
         }
     }
 }
+#endif
